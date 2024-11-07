2ddc574c3628de33b1679d0d01ffe53ab299fbf6
Romika <romika2002chau.21@gmail.com>
2024-11-07 00:42:23 -0500

    Solved binary search

--- binarySearch.js
+++ binarySearch.js
@@ -1,8 +1,16 @@
 function binarySearch(t) {
-    let myArr = [-5, 2, 4, 6, 10];
-    for (let i = 0; i < myArr.length; i++) {
-        if (myArr[i] === t) {
-            return i
+    let arr = [-5, 2, 4, 6, 10];
+    let left_index = 0;
+    let right_index = arr.length - 1;
+    while (left_index <= right_index) {
+        let middle_index = Math.floor((left_index + right_index) / 2);
+        if (t === arr[middle_index]) {
+            return middle_index
+        }
+        if (t < arr[middle_index]) {
+            right_index = middle_index - 1;
+        } else {
+            left_index = middle_index + 1;
         }
     }
     return -1
