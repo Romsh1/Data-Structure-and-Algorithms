bff7682c37f14cb079180767bb324202a18c3d51
Romika <romika2002chau.21@gmail.com>
2024-11-25 01:05:59 -0500

    Linked list implementation with Queue data structure

--- LinkedList/index.js
+++ LinkedList/index.js
@@ -1,5 +1,6 @@
 const LinkedList = require('././linkedListStack') 
 
+/*
 class LinkedListStack {
     constructor() {
         this.list = new LinkedList()
@@ -37,4 +38,47 @@ stack.push(10)
 stack.push(30)
 stack.print()
 console.log(stack.peek())
-console.log(stack.pop())
+console.log(stack.pop()) */
+
+//Linked List Queue
+class LinkedListQueue {
+    constructor() {
+        this.list = new LinkedList()
+    }
+
+    enqueue(value) {
+        this.list.append(value)
+    }
+
+    dequeue() {
+        return this.list.removeFromFront()
+    }
+
+    peek() {
+        return this.list.head.value
+    }
+
+    isEmpty() {
+        return this.list.isEmpty()
+    }
+
+    getSize() {
+        return this.list.getSize()
+    }
+
+    print() {
+        return this.list.print()
+    }
+}
+
+const queue = new LinkedListQueue()
+console.log(queue.isEmpty())
+queue.enqueue(10)
+queue.enqueue(20)
+queue.enqueue(30)
+console.log(queue.getSize())
+queue.print()
+
+console.log(queue.dequeue())
+queue.print()
+console.log(queue.peek())
