003074ef5f16550b46e189cff620e65f5ff62a15
Romika <romika2002chau.21@gmail.com>
2024-11-26 01:37:41 -0500

    Implemented bst search

--- Tree/tree.js
+++ Tree/tree.js
@@ -14,7 +14,57 @@ class BinarySearchTree {
     isEmpty() {
         return this.root === null
     }
+
+    insert(value) {
+        //creating a node to insert value
+        const newNode = new Node(value)
+        if(this.isEmpty()) {
+            this.root = newNode
+        } else {
+            this.insertNode(this.root, newNode)
+        }
+    }
+
+    insertNode(root, newNode) {
+        //Checking for left subtree
+        if(newNode.value < root.value) {
+            if(root.left === null) {
+                root.left = newNode
+            } else {
+                this.insertNode(root.left, newNode)
+            }
+        } 
+        //checking for right subtree
+        else {
+            if(root.right === null) {
+                root.right = newNode
+            } else {
+                this.insertNode(root.right, newNode)
+            }
+        }
+    } 
+
+    search(root, value) {
+        if(!root) {
+            return false
+        } else {
+            if(root.value === value) {
+                return true
+            } else if(value < root.value) {
+                return this.search(root.left, value)
+            } else {
+                return this.search(root.right, value)
+            }
+        }
+    }
+    
 }
 
 const bst = new BinarySearchTree()
 console.log('Tree is empty?', bst.isEmpty())
+bst.insert(10)
+bst.insert(5)
+bst.insert(15)
+console.log(bst.search(bst.root, 10))
+console.log(bst.search(bst.root, 5))
+console.log(bst.search(bst.root, 25))
