f8e1a00c4e14a830fe63ae2726f22fcb1ef03594
Romika <romika2002chau.21@gmail.com>
2024-11-26 16:51:21 -0500

    Found out the maximum and minimum value

--- Tree/tree.js
+++ Tree/tree.js
@@ -96,6 +96,22 @@ class BinarySearchTree {
             }
         }
     }
+
+    min(root) {
+        if(!root.left) {
+            return root.value
+        } else {
+            return this.min(root.left)
+        }
+    }
+
+    max(root) {
+        if(!root.right) {
+            return root.value
+        } else {
+            return this.max(root.right)
+        }
+    }
 }
 
 const bst = new BinarySearchTree()
@@ -112,3 +128,6 @@ console.log(bst.search(bst.root, 25))
 bst.preOrder(bst.root)
 bst.inOrder(bst.root)
 bst.levelOrder(bst.root)
+
+console.log(bst.min(bst.root))
+console.log(bst.max(bst.root))
