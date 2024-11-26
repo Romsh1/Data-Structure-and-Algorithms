d0f638d8ea8bbeb53273e8b51660c35e9dfa7a8f
Romika <romika2002chau.21@gmail.com>
2024-11-26 00:17:10 -0500

    Hash table implementation in JavaScript

--- HashTable/hashTable.js
+++ HashTable/hashTable.js
@@ -15,17 +15,44 @@ class HashTable {
 
     set(key, value) {
         const index = this.hash(key)
-        this.table[index] = value
+        //this.table[index] = value
+        const bucket = this.table[index]
+        if(!bucket) {
+            this.table[index] = [[key, value]]
+        } else {
+            const sameKeyItem = bucket.find(item => item[0] === key)
+            if(sameKeyItem) {
+                sameKeyItem[1] = value
+            } else {
+                bucket.push([key, value])
+            }
+        }
     }
 
     get(key) {
         const index = this.hash(key)
-        return this.table[index]
+        //return this.table[index]
+        const bucket = this.table[index]
+        if(bucket) {
+            const sameKeyItem = bucket.find(item => item[0] === key)
+            if(sameKeyItem) {
+                //key is stored at index 0 and value at 1
+                return sameKeyItem[1]
+            }
+        }
+        return undefined
     }
 
     remove(key) {
         const index = this.hash(key)
-        this.table[index] = undefined
+        //this.table[index] = undefined
+        const bucket = this.table[index]
+        if(bucket) {
+            const sameKeyItem = bucket.find(item => item[0] === key)
+            if(sameKeyItem) {
+                bucket.splice(bucket.indexOf(sameKeyItem), 1)
+            }
+        }
     }
 
     display() {
@@ -42,5 +69,7 @@ table.set("name", "Lily")
 table.set("age", 23)
 table.display()
 console.log(table.get("name"))
+table.set("mane", "Boo")
+table.set("name", "Lincoln")
 table.remove("name")
 table.display()
