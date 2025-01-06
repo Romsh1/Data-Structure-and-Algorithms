565510a667ba5d4b21db902fa1f84cd4f303634e
Romika <romika2002chau.21@gmail.com>
2025-01-06 00:45:55 -0500

    Implemented Quick sort to sort am array in aescending order when low and high are given as params

--- concepts/quickSort.js
+++ concepts/quickSort.js
@@ -43,3 +43,52 @@ function quickSort(arr) {
 
 const arr = [8, 20, -2, 4, -6];
 console.log(quickSort(arr));
+
+
+
+/* GeeksforGeeks */
+/*
+    Quick sort when low and high are given as params
+*/
+
+/**
+ * @param {number[]} arr
+ * @param {number} low
+ * @param {number} high
+ */
+
+class Solution {
+    partition(arr, low, high) {
+        // Your code here
+        let pivot = arr[high];
+        
+        //starting index for i from -1
+        let i = low - 1;
+        
+        //traversing through each element
+        for (let j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                //swapping
+                [arr[i], arr[j]] = [arr[j], arr[i]];
+            }
+        }
+        
+        [arr[i + 1], arr[high]] = [arr[high], arr[i + 1]];
+        return i + 1; //returning index
+        
+    }
+    
+
+    quickSort(arr, low, high) {
+        if (low < high) {
+
+        // pi is the partition return index of pivot
+        let pi = this.partition(arr, low, high);
+
+        this.quickSort(arr, low, pi - 1);
+        this.quickSort(arr, pi + 1, high);
+        }
+    }
+
+}
